3da4b84edc689331efeee602030a4aabffaaedd9
Leechael Yim <yanleech@gmail.com>
2025-02-05 16:56:53 +0800

    feat: refactor get_quote_collateral & returns collateral in verify API as well.

--- dcap-attestation/src/dcap_attestation/api.py
+++ dcap-attestation/src/dcap_attestation/api.py
@@ -15,14 +15,6 @@ from .database import get_db
 from .verify import verify_quote_with_collateral
 
 
-class VerificationResponse(BaseModel):
-    success: bool
-    quote: Optional[Quote] = None
-    checksum: Optional[str] = None
-    can_download: Optional[bool] = None
-    uploaded_at: Optional[str] = None
-
-
 class QuoteCollateralV3(BaseModel):
     tcb_info_issuer_chain: str
     tcb_info: str
@@ -30,6 +22,45 @@ class QuoteCollateralV3(BaseModel):
     qe_identity_issuer_chain: str
     qe_identity: str
     qe_identity_signature: str
+
+
+class VerificationResponse(BaseModel):
+    success: bool
+    quote: Optional[Quote] = None
+    checksum: Optional[str] = None
+    can_download: Optional[bool] = None
+    uploaded_at: Optional[str] = None
+    quote_collateral: Optional[QuoteCollateralV3] = None
+
+
+async def get_quote_collateral(quote: Quote):
+    async def get_tcb_from_fmspc(fmspc):
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(f'https://api.trustedservices.intel.com/tdx/certification/v4/tcb?fmspc={fmspc}')
+            tcb_info_issuer_chain = resp.headers.get('SGX-TCB-Info-Issuer-Chain')
+            if not tcb_info_issuer_chain:
+                tcb_info_issuer_chain = resp.headers.get('TCB-Info-Issuer-Chain')
+            return (unquote(tcb_info_issuer_chain), resp.json())
+
+    async def get_qe_identity():
+        async with httpx.AsyncClient() as client:
+            resp = await client.get('https://api.trustedservices.intel.com/tdx/certification/v4/qe/identity')
+            qe_identity_issuer_chain = resp.headers.get('SGX-Enclave-Identity-Issuer-Chain')
+            return (unquote(qe_identity_issuer_chain), resp.json())
+
+    ((tcb_info_issuer_chain, tcb_info), (qe_identity_issuer_chain, qe_identity)) = await asyncio.gather(
+        get_tcb_from_fmspc(quote.certificate_chain[0].sgx_extensions.fmspc),
+        get_qe_identity()
+    )
+
+    return QuoteCollateralV3(
+        tcb_info_issuer_chain=tcb_info_issuer_chain,
+        tcb_info=json.dumps(tcb_info.get('tcbInfo'), separators=(',', ':')),
+        tcb_info_signature=tcb_info.get('signature'),
+        qe_identity_issuer_chain=qe_identity_issuer_chain,
+        qe_identity=json.dumps(qe_identity.get('enclaveIdentity'), separators=(',', ':')),
+        qe_identity_signature=qe_identity.get('signature'),
+    )
 
 
 app = FastAPI(root_path='/api/attestations')
@@ -44,6 +75,8 @@ async def verify(file: UploadFile = File(...), db: Session = Depends(get_db)):
         row = crud.create_quote(db, quote)
         crud.save_raw_quote(db, row, content)
         record.checksum = row.checksum
+        if quote.verified:
+            record.quote_collateral = await get_quote_collateral(quote)
     return JSONResponse(content=record.model_dump())
 
 @app.get('/recent')
@@ -72,35 +105,7 @@ async def get_collateral(checksum: str, db: Session = Depends(get_db)):
     if not row:
         raise HTTPException(status_code=404, detail="Not found")
     quote = row.to_instance()
-
-    async def get_tcb_from_fmspc(fmspc):
-        async with httpx.AsyncClient() as client:
-            resp = await client.get(f'https://api.trustedservices.intel.com/tdx/certification/v4/tcb?fmspc={fmspc}')
-            tcb_info_issuer_chain = resp.headers.get('SGX-TCB-Info-Issuer-Chain')
-            if not tcb_info_issuer_chain:
-                tcb_info_issuer_chain = resp.headers.get('TCB-Info-Issuer-Chain')
-            return (unquote(tcb_info_issuer_chain), resp.json())
-
-    async def get_qe_identity():
-        async with httpx.AsyncClient() as client:
-            resp = await client.get('https://api.trustedservices.intel.com/tdx/certification/v4/qe/identity')
-            qe_identity_issuer_chain = resp.headers.get('SGX-Enclave-Identity-Issuer-Chain')
-            return (unquote(qe_identity_issuer_chain), resp.json())
-
-    ((tcb_info_issuer_chain, tcb_info), (qe_identity_issuer_chain, qe_identity)) = await asyncio.gather(
-        get_tcb_from_fmspc(quote.certificate_chain[0].sgx_extensions.fmspc),
-        get_qe_identity()
-    )
-
-    collateral = QuoteCollateralV3(
-        tcb_info_issuer_chain=tcb_info_issuer_chain,
-        tcb_info=json.dumps(tcb_info.get('tcbInfo'), separators=(',', ':')),
-        tcb_info_signature=tcb_info.get('signature'),
-        qe_identity_issuer_chain=qe_identity_issuer_chain,
-        qe_identity=json.dumps(qe_identity.get('enclaveIdentity'), separators=(',', ':')),
-        qe_identity_signature=qe_identity.get('signature'),
-    )
-
+    collateral = await get_quote_collateral(quote)
     return JSONResponse(
         content=collateral.model_dump(),
         headers={
